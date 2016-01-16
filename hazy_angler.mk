247fb894e4c1b88b72d4d45237f499bf5be6d577
Salvatore Festa <salvatorefestatech@gmail.com>
2016-01-16 17:45:22 +0100

    Added hazy_angler makefile

---
+++ hazy_angler.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Hazy stuff.
+$(call inherit-product, vendor/hazy/configs/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/hazy/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := hazy_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
